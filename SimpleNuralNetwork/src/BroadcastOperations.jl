30e4de2d2e8612f3616b403c7eee9a3de65219f3
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-06-01 22:18:00 +0200

    Initial working neural network training

---
+++ SimpleNuralNetwork/src/BroadcastOperations.jl
@@ -0,0 +1,51 @@
+import Base: *
+import LinearAlgebra: mul!
+# x * y (aka matrix multiplication)
+*(A::GraphNode, x::GraphNode) = BroadcastedOperator(mul!, A, x)
+forward(::BroadcastedOperator{typeof(mul!)}, A, x) = return A * x
+backward(::BroadcastedOperator{typeof(mul!)}, A, x, g) = tuple(g * x', A' * g)
+
+# x .* y (element-wise multiplication)
+Base.Broadcast.broadcasted(*, x::GraphNode, y::GraphNode) = BroadcastedOperator(*, x, y)
+forward(::BroadcastedOperator{typeof(*)}, x, y) = return x .* y
+backward(node::BroadcastedOperator{typeof(*)}, x, y, g) = let
+    𝟏 = ones(length(node.output))
+    Jx = diagm(y .* 𝟏)
+    Jy = diagm(x .* 𝟏)
+    tuple(Jx' * g, Jy' * g)
+end
+
+Base.Broadcast.broadcasted(-, x::GraphNode, y::GraphNode) = BroadcastedOperator(-, x, y)
+forward(::BroadcastedOperator{typeof(-)}, x, y) = return x .- y
+backward(::BroadcastedOperator{typeof(-)}, x, y, g) = tuple(g,-g)
+
+Base.Broadcast.broadcasted(+, x::GraphNode, y::GraphNode) = BroadcastedOperator(+, x, y)
+forward(::BroadcastedOperator{typeof(+)}, x, y) = return x .+ y
+backward(::BroadcastedOperator{typeof(+)}, x, y, g) = tuple(g, g)
+
+import Base: sum
+sum(x::GraphNode) = BroadcastedOperator(sum, x)
+forward(::BroadcastedOperator{typeof(sum)}, x) = return sum(x)
+backward(::BroadcastedOperator{typeof(sum)}, x, g) = let
+    𝟏 = ones(length(x))
+    J = 𝟏'
+    tuple(J' * g)
+end
+
+Base.Broadcast.broadcasted(/, x::GraphNode, y::GraphNode) = BroadcastedOperator(/, x, y)
+forward(::BroadcastedOperator{typeof(/)}, x, y) = return x ./ y
+backward(node::BroadcastedOperator{typeof(/)}, x, y::Real, g) = let
+    𝟏 = ones(length(node.output))
+    Jx = diagm(𝟏 ./ y)
+    Jy = (-x ./ y .^2)
+    tuple(Jx' * g, Jy' * g)
+end
+
+import Base: max
+Base.Broadcast.broadcasted(max, x::GraphNode, y::GraphNode) = BroadcastedOperator(max, x, y)
+forward(::BroadcastedOperator{typeof(max)}, x, y) = return max.(x, y)
+backward(::BroadcastedOperator{typeof(max)}, x, y, g) = let
+    Jx = diagm(isless.(y, x))
+    Jy = diagm(isless.(x, y))
+    tuple(Jx' * g, Jy' * g)
+end
---
+++ SimpleNuralNetwork/src/ScalarOperations.jl
@@ -0,0 +1,10 @@
+import Base: ^
+^(x::GraphNode, n::GraphNode) = ScalarOperator(^, x, n)
+forward(::ScalarOperator{typeof(^)}, x, n) = return x^n
+backward(::ScalarOperator{typeof(^)}, x, n, g) = tuple(g * n * x ^ (n-1), g * log(abs(x)) * x ^ n)
+
+import Base: sin
+sin(x::GraphNode) = ScalarOperator(sin, x)
+forward(::ScalarOperator{typeof(sin)}, x) = return sin(x)
+backward(::ScalarOperator{typeof(sin)}, x, g) = tuple(g * cos(x))
+
